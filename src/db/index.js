42489d1fa106346376e2ba37cbf54d5f5584ff7b
Mason <kevmace18@gmail.com>
2020-04-30 13:40:18 +0200

    added git ingnore & db

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCWMcjaM4tQer-_228g1c20c7uwOY4u14",
+  authDomain: "leader-board-c3385.firebaseapp.com",
+  databaseURL: "https://leader-board-c3385.firebaseio.com",
+  projectId: "leader-board-c3385",
+  storageBucket: "leader-board-c3385.appspot.com",
+  messagingSenderId: "964919982972",
+  appId: "1:964919982972:web:cb1b6875d2567a818042f9",
+  measurementId: "G-MQZKC21ZQM"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+// npm install firebase
