e5783f1890140cd118f112da4d4acd67e2a567ef
Mason <kevmace18@gmail.com>
2020-05-03 00:25:47 +0200

    cleanup exports from reducer

--- src/reducer/helpers/index.js
+++ src/reducer/helpers/index.js
@@ -1,5 +1,1 @@
 export { default as logIn } from './log-in'
-// export { default as refreshContestants } from "./refresh-contestants";
-// export { default as refreshPoints } from "./refresh-points";
-// export { default as removeContestant } from "./remove-contestant";
-// export { default as submitContestants } from "./submit-contestants";
